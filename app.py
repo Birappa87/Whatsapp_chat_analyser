1c46026518ecb7a726a9f81606c56ca1baefab4a
Birappa Goudanavar <birappa001@gmail.com>
2022-08-09 20:59:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -52,18 +52,6 @@ if uploaded_file is not None:
             st.subheader("Total Links shared")
             st.subheader(links)
 
-        st.title('Emoji Analysis')
-        col1, col2 = st.columns(2)
-
-        with col1:
-            st.dataframe(emoji_df)
-
-        with col2:
-            fig, ax = plt.subplots()
-            ax.pie(emoji_df[1].head(), labels = emoji_df[0].head(), autopct="%0.2f")
-            st.pyplot(fig)
-
-
 
         # Finding busiest person
         if selected_user == 'Overall':
